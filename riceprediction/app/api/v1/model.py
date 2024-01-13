5cc7999c9dc4a98784c2f750b728753ef5afd058
shenzijie <shenzijie2013@163.com>
2024-01-14 00:35:07 +0800

    "增加predict和ML project的错误提示"

--- riceprediction/app/api/v1/model.py
+++ riceprediction/app/api/v1/model.py
@@ -12,7 +12,7 @@ import pandas as pd
 # import plotly.express as px
 from lightgbm import LGBMRegressor
 from sklearn.metrics import mean_squared_error
-from flask import render_template, request, after_this_request, send_file, current_app
+from flask import render_template, request, after_this_request, send_file, flash
 from app.libs.redprint import Redprint
 from app.validations.lgbPara import SearchSpace
 
@@ -48,70 +48,77 @@ def get_k_fold_data(k, i, X, y):
 def model():
     search_space = SearchSpace()
     if request.method == "POST" and search_space.validate_on_submit():
-# 读取 CSV 文件，指定 'sample' 列为索引列
-        features = pd.read_csv(search_space.features.data, index_col='sample')
+        # 读取 CSV 文件，指定 'sample' 列为索引列
+        try:
+            features = pd.read_csv(search_space.features.data, index_col='sample')
 
-        # 获取特征列和标签列
-        X = features.drop(columns=['labels'])  # 剩余的列为特征列
-        y = features['labels']  # 'label' 列为标签列
-        x_train, y_train, x_valid, y_valid = get_k_fold_data(5, 1, X.values, y.values)
-        kwargs = search_space
+            # 获取特征列和标签列
+            X = features.drop(columns=['labels'])  # 剩余的列为特征列
+            y = features['labels']  # 'label' 列为标签列
+            x_train, y_train, x_valid, y_valid = get_k_fold_data(5, 1, X.values, y.values)
+            kwargs = search_space
 
-        def objective(trial):
-            param = {
-                'metric': kwargs.metric.data.strip(), 
-                "boosting_type": kwargs.boosting_type.data.strip(),                
-                "seed": kwargs.seed.data,
-                "verbosity": -1,
-                "n_estimators": trial.suggest_int('n_estimators', kwargs.n_estimators_min.data, kwargs.n_estimators_max.data),
-                "learning_rate": trial.suggest_float("learning_rate", kwargs.learning_rate_min.data, kwargs.learning_rate_max.data),
-                "max_depth": trial.suggest_int("max_depth", kwargs.max_depth_min.data, kwargs.max_depth_max.data),
-                'num_leaves': trial.suggest_int('num_leaves', kwargs.leaves_min.data, kwargs.leaves_max.data),
-                'min_child_samples': trial.suggest_int('min_child_samples', kwargs.min_child_min.data, kwargs.min_child_max.data),
-                'feature_fraction': trial.suggest_uniform('feature_fraction', kwargs.features_fraction_min.data, kwargs.features_fraction_max.data),
-                'bagging_fraction': trial.suggest_uniform('bagging_fraction', kwargs.bagging_fraction_min.data, kwargs.bagging_fraction_max.data),
-                'lambda_l1': trial.suggest_loguniform('lambda_l1', kwargs.lambda_l1_min.data, kwargs.lambda_l1_max.data),
-                'lambda_l2': trial.suggest_loguniform('lambda_l2', kwargs.lambda_l2_min.data, kwargs.lambda_l2_max.data),
-            }
-            lgb=LGBMRegressor(**param)
-            pruning_callback = optuna.integration.LightGBMPruningCallback(trial, 'l2')
-            lgb.fit(x_train, y_train, eval_set=[(x_valid, y_valid)],callbacks=[pruning_callback])
-            preds = lgb.predict(x_valid)
-            accuracy = np.sqrt(mean_squared_error(y_valid, preds))
-            return accuracy
-        
+            def objective(trial):
+                param = {
+                    'metric': kwargs.metric.data.strip(), 
+                    "boosting_type": kwargs.boosting_type.data.strip(),                
+                    "seed": kwargs.seed.data,
+                    "verbosity": -1,
+                    "n_estimators": trial.suggest_int('n_estimators', kwargs.n_estimators_min.data, kwargs.n_estimators_max.data),
+                    "learning_rate": trial.suggest_float("learning_rate", kwargs.learning_rate_min.data, kwargs.learning_rate_max.data),
+                    "max_depth": trial.suggest_int("max_depth", kwargs.max_depth_min.data, kwargs.max_depth_max.data),
+                    'num_leaves': trial.suggest_int('num_leaves', kwargs.leaves_min.data, kwargs.leaves_max.data),
+                    'min_child_samples': trial.suggest_int('min_child_samples', kwargs.min_child_min.data, kwargs.min_child_max.data),
+                    'feature_fraction': trial.suggest_uniform('feature_fraction', kwargs.features_fraction_min.data, kwargs.features_fraction_max.data),
+                    'bagging_fraction': trial.suggest_uniform('bagging_fraction', kwargs.bagging_fraction_min.data, kwargs.bagging_fraction_max.data),
+                    'lambda_l1': trial.suggest_loguniform('lambda_l1', kwargs.lambda_l1_min.data, kwargs.lambda_l1_max.data),
+                    'lambda_l2': trial.suggest_loguniform('lambda_l2', kwargs.lambda_l2_min.data, kwargs.lambda_l2_max.data),
+                }
+                lgb=LGBMRegressor(**param)
+                pruning_callback = optuna.integration.LightGBMPruningCallback(trial, 'l2')
+                lgb.fit(x_train, y_train, eval_set=[(x_valid, y_valid)],callbacks=[pruning_callback])
+                preds = lgb.predict(x_valid)
+                accuracy = np.sqrt(mean_squared_error(y_valid, preds))
+                return accuracy
+            
 
-        optuna.logging.set_verbosity(optuna.logging.WARNING) # 压缩报告信息
-        study_tuner = optuna.create_study(direction='minimize', pruner=optuna.pruners.MedianPruner())
-        study_tuner.optimize(objective, n_trials=search_space.trails.data)
+            optuna.logging.set_verbosity(optuna.logging.WARNING) # 压缩报告信息
+            study_tuner = optuna.create_study(direction='minimize', pruner=optuna.pruners.MedianPruner())
+            study_tuner.optimize(objective, n_trials=search_space.trails.data)
 
-        # 绘制optuna训练历史
-        # fig_history = figjson(optuna.visualization.plot_optimization_history(study_tuner))
-        # fig_para = figjson(optuna.visualization.plot_param_importances(study_tuner))
+            # 绘制optuna训练历史
+            # fig_history = figjson(optuna.visualization.plot_optimization_history(study_tuner))
+            # fig_para = figjson(optuna.visualization.plot_param_importances(study_tuner))
 
-        lgb = LGBMRegressor(**study_tuner.best_params)
-        lgb.fit(x_train, y_train, eval_set=[(x_valid, y_valid)])
+            lgb = LGBMRegressor(**study_tuner.best_params)
+            lgb.fit(x_train, y_train, eval_set=[(x_valid, y_valid)])
 
-        ## 绘制最有模型中的重要特征
-        # feature_importance = pd.DataFrame(lgb.feature_importances_,
-        #                                     index=X.columns.values,
-        #                                     columns=['feature_importance']).sort_values('feature_importance').tail(10)
-        # fig_features = figjson(px.bar(feature_importance, orientation='h', height=400))
-        # return render_template('predict_results.html', graphJSON_history=fig_history, graphJSON_paras=fig_para, graphJSON_features=fig_features)
+            ## 绘制最有模型中的重要特征
+            # feature_importance = pd.DataFrame(lgb.feature_importances_,
+            #                                     index=X.columns.values,
+            #                                     columns=['feature_importance']).sort_values('feature_importance').tail(10)
+            # fig_features = figjson(px.bar(feature_importance, orientation='h', height=400))
+            # return render_template('predict_results.html', graphJSON_history=fig_history, graphJSON_paras=fig_para, graphJSON_features=fig_features)
 
-        
-        # 最优模型返回
-        filename = generate_name()
-        joblib.dump(lgb, filename)
-        @after_this_request
-        def remove_file(response):
-            os.remove(filename)
-            return response
-        return send_file(
-            filename,
-            mimetype='application/octet-stream',
-            as_attachment=True
-        )
+            
+            # 最优模型返回
+            filename = generate_name()
+            joblib.dump(lgb, filename)
+            @after_this_request
+            def remove_file(response):
+                os.remove(filename)
+                return response
+            return send_file(
+                filename,
+                mimetype='application/octet-stream',
+                as_attachment=True
+            )
+        except Exception as e:
+            if "sample" in str(e):
+                flash("Your data may not have 'sample' header specifying the sample name.", 'error')
+            else:
+            # 其他异常情况
+                flash(f"Error: {str(e)}", "error")
 
     return render_template('model.html', space=search_space)
 
--- riceprediction/app/api/v1/predict.py
+++ riceprediction/app/api/v1/predict.py
@@ -1,7 +1,7 @@
 import os, joblib
 import numpy as np
 import pandas as pd
-from flask import render_template, request, url_for
+from flask import render_template, request, flash
 from app.libs.redprint import Redprint
 from app.validations.upload import UploadFile
 
@@ -16,14 +16,23 @@ def predict():
     fileform = UploadFile()
     if request.method == 'POST' and fileform.validate_on_submit():
         headers = pd.read_csv(cloned_gene)
-        features_file = pd.read_csv(fileform.file.data, index_col='sample')
-        samples = features_file.index.tolist()
-        features = features_file[headers['0'].values.tolist()].values
-        features = features.reshape(-1, 1, features.shape[1]).astype(np.float32)
-        gw_model = joblib.load(gru_gw_path)  # 加载训练好的模型
-        gl_model = joblib.load(gru_gl_path)
-        gw_result = gw_model.predict(features)
-        gl_result = gl_model.predict(features)
-        result_data = [{'sample': sample, 'gw': gw, 'gl': gl} for sample, gw, gl in zip(samples, gw_result, gl_result)]
-        return render_template('predict_result.html', result_data=result_data)
+        try:
+            features_file = pd.read_csv(fileform.file.data, index_col='sample')
+            samples = features_file.index.tolist()
+            features = features_file[headers['0'].values.tolist()].values
+            features = features.reshape(-1, 1, features.shape[1]).astype(np.float32)
+            gw_model = joblib.load(gru_gw_path)  # 加载训练好的模型
+            gl_model = joblib.load(gru_gl_path)
+            gw_result = gw_model.predict(features)
+            gl_result = gl_model.predict(features)
+            result_data = [{'sample': sample, 'gw': gw, 'gl': gl} for sample, gw, gl in zip(samples, gw_result, gl_result)]
+            return render_template('predict_result.html', result_data=result_data)
+        except Exception as e:
+            if "sample" in str(e):
+                flash("Your data may not have 'sample' header specifying the sample name.", 'error')
+            elif "not in index" in str(e):
+                flash("Your data may be not gsctool features.", 'error')
+            else:
+            # 其他异常情况
+                flash(f"Error: {str(e)}", "error")
     return render_template('predict.html', form=fileform)
